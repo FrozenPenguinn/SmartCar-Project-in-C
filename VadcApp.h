17f39203f5a35edd66f9258d0fc959b98829fe37
Frozen Penguinn <47574375+FrozenPenguinn@users.noreply.github.com>
2020-01-16 16:45:50 +0800

    Update VadcApp.h

--- VadcApp.h
+++ VadcApp.h
@@ -9,7 +9,7 @@
 #define VADCAPP_H_
 #include "Vadc/Adc/IfxVadc_Adc.h"
 #include "IfxVadc_regdef.h"
-void VADC_init(void);
-extern void VADCresult_run(void);//��ȡAD���
-extern unsigned int  VADCresult[9];//AD����洢λ�� 0.0---0.8 AN0....AN8
+void VADC_Init(void);
+extern void VADC_Result_Run(void);//��ȡAD���
+extern unsigned int  VADC_result[9];//AD����洢λ�� 0.0---0.8 AN0....AN8
 #endif /* 0_SRC_0_APPSW_TRICORE_API_VADC_VADCAPP_H_ */
