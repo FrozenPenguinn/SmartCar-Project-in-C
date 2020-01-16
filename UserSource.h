ba89e89a1d7a9328cbe66f885ba1e60ffb784c4b
Frozen Penguinn <47574375+FrozenPenguinn@users.noreply.github.com>
2020-01-16 16:43:34 +0800

    Update UserSource.h

--- UserSource.h
+++ UserSource.h
@@ -1,7 +1,7 @@
 /*
  * UserSource.h
  *
- *  Created on: 2019��1��7��
+ *  Created on: 2019/1/7
  *      Author: ZHANGZH
  */
 
@@ -9,12 +9,18 @@
 #define _USERSOURCE_H_
 
 #include "ServeSource.h"
+IFX_EXTERN void Motor_Duty(int duty);
+IFX_EXTERN void Steer_Angle(int duty);
+IFX_EXTERN double PID_Sim(int mag);
+IFX_EXTERN int Speed_Control(int speed_want);
+IFX_EXTERN int* Avg_Filter(void);
+IFX_EXTERN void Run(void);
 IFX_EXTERN void UserCpu0Main(void);
 IFX_EXTERN void UserCpu1Main(void);
 IFX_EXTERN uint32 UserInterupt10ms(void);
 IFX_EXTERN uint32 UserInterupt100ms(void);
 IFX_EXTERN uint32 UserInterupt1000ms(void);
 IFX_EXTERN void UserInteruptIO(void);
-IFX_EXTERN int* avg_filter(void)
-IFX_EXTERN void run(void);
+
+
 #endif /* 0_SRC_APPSW_TRICORE_MAIN_USERSOURCE_H_ */
